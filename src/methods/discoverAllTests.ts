aecbc3d5b104f34ccc955a342a387e6d3c353fed
Robin Heidenis <robin.heidenis@gmail.com>
2023-05-26 21:19:58 +0200

    feat: 🎸 implemented passing standard set of options to runner
    ✅ Closes: #11

--- src/methods/discoverAllTests.ts
+++ src/methods/discoverAllTests.ts
@@ -1,20 +1,21 @@
 import { BaseTestSuite, PartialTestRunnerOptions } from "@isildur-testing/api";
 import jest from "jest";
+import { getJestOptions } from "~/helpers/getJestOptions.js";
 import { parseDiscoveredSuite } from "~/helpers/parseSuite.js";
-import { WEIRD_JEST_DEFAULT_CONFIG } from "~/helpers/weirdJestDefaultConfig.js";
 const { runCLI } = jest;
 
 export const discoverAllTests = async (
   options?: PartialTestRunnerOptions
 ): Promise<BaseTestSuite[]> => {
-  const mergedOptions = {
-    ...WEIRD_JEST_DEFAULT_CONFIG,
-    projects: [process.cwd()],
-    config: options?.config,
-    testNamePattern: "CrazyTestNameThatWillNeverMatchAnything",
-  } satisfies Parameters<typeof runCLI>[0];
-
-  const result = await runCLI(mergedOptions, mergedOptions.projects ?? []);
+  const jestOptions = getJestOptions(options);
+  const result = await runCLI(
+    {
+      ...jestOptions,
+      testNamePattern: "crazyTestNamePatternThatWillNeverMatchAnything", // This is the only way to ensure no tests get executed, but we still get the discovered tests
+      debug: true,
+    },
+    jestOptions.projects ?? []
+  );
 
   return result.results.testResults.flatMap((suite) =>
     parseDiscoveredSuite(suite)
--- src/methods/runAllTests.ts
+++ src/methods/runAllTests.ts
@@ -1,19 +1,14 @@
 import { PartialTestRunnerOptions, TestSuite } from "@isildur-testing/api";
 import jest from "jest";
+import { getJestOptions } from "~/helpers/getJestOptions.js";
 import { parseRanSuite } from "~/helpers/parseSuite.js";
-import { WEIRD_JEST_DEFAULT_CONFIG } from "~/helpers/weirdJestDefaultConfig.js";
 const { runCLI } = jest;
 
 export const runAllTests = async (
   options?: PartialTestRunnerOptions
 ): Promise<TestSuite[]> => {
-  const mergedOptions = {
-    ...WEIRD_JEST_DEFAULT_CONFIG,
-    projects: [process.cwd()],
-    config: options?.config,
-  } satisfies Parameters<typeof runCLI>[0];
-
-  const result = await runCLI(mergedOptions, mergedOptions.projects ?? []);
+  const jestOptions = getJestOptions(options);
+  const result = await runCLI(jestOptions, jestOptions.projects ?? []);
 
   return result.results.testResults.flatMap((suite) => parseRanSuite(suite));
 };
